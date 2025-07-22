d5a6a2d05e183a04be3c1abae80e6c3646c67c0b
Ismael-Martinez_atsolera <Ismael.Martinez@Solera.com>
2025-07-22 11:42:27 -0600

    Services and controllers updates

--- src/main/java/com/SoleraBootcamp/PingPongAssesment/Controllers/VehiclesController.java
+++ src/main/java/com/SoleraBootcamp/PingPongAssesment/Controllers/VehiclesController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.SoleraBootcamp.PingPongAssesment.Model.Vehicles;
 import com.SoleraBootcamp.PingPongAssesment.Repository.VehiclesRepository;
+import com.SoleraBootcamp.PingPongAssesment.Services.VehiclesService;
+import com.SoleraBootcamp.PingPongAssesment.Services.WorkshopService;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +18,6 @@ import java.util.Optional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -23,59 +25,42 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/api")
 public class VehiclesController {
 
-    private final VehiclesRepository vehiclesRepository;
-    public VehiclesController(VehiclesRepository repository){
-        vehiclesRepository = repository;
+     private final VehiclesService vehiclesService;
+
+
+    public VehiclesController(VehiclesService vehiclesService){
+        this.vehiclesService = vehiclesService;
     }
 
     // Endpoint to create a new workshop
     @PostMapping("/vehicles")
     public String createVehicle(@RequestBody Vehicles vehicle) {
-        vehiclesRepository.save(vehicle);
-        return "The vehicle with the model " + vehicle.getModel() + " has been created";
+        return vehiclesService.createVehicle(vehicle);
     }
     
     //Endpoint to get a vehicle by ID
     @GetMapping("/vehicles/{id}")
     public Vehicles getVehicle(@PathVariable(value="id") Long Id) {
-        Optional<Vehicles> optionalVehicle = vehiclesRepository.findById(Id);
-        if(optionalVehicle.isEmpty()) {
-            return null; // or throw an exception
-        } else {
-            Vehicles vehicle = optionalVehicle.get();
-            return vehicle;
-        }
+        return vehiclesService.getVehicle(Id);
     }
 
     //Endpoint to get all vehicles
     @GetMapping("/allvehicles")
     public List<Vehicles> getAllVehicles() {
-        return (List<Vehicles>) vehiclesRepository.findAll();
+        return vehiclesService.getAllVehicles();
     }
 
     //Endpoint to delete a vehicle by ID
     @DeleteMapping("/vehicles/{id}")
     public String deleteVehicleById(@PathVariable(value="id") Long Id) {
-        vehiclesRepository.deleteById(Id);
-        return "Vehicle with ID " + Id + " has been deleted";
+        return vehiclesService.deleteVehicleById(Id);
+
     }
 
     //Endpoint to edit a vehicle by ID
     @PutMapping("/vehicles/{id}")
     public String updateVehicleById(@RequestBody Vehicles vehicleDetails, @PathVariable(value="id") Long Id) {
-        Optional<Vehicles> optionalVehicle = vehiclesRepository.findById(Id);
-        if(optionalVehicle.isEmpty()) {
-            return "Vehicle with ID " + Id + " not found";
-        } else {
-            Vehicles vehicle = optionalVehicle.get();
-            vehicle.setModel(vehicleDetails.getModel());
-            vehicle.setBrand(vehicleDetails.getBrand());
-            vehicle.setVehicleYear(vehicleDetails.getVehicleYear());
-            vehicle.setColor(vehicleDetails.getColor());
-            vehicle.setVin(vehicleDetails.getVin());
-            vehiclesRepository.save(vehicle);
-            return "Vehicle with ID " + Id + " has been updated";
-        }
+                return vehiclesService.updateVehicleById(vehicleDetails, Id);
     }
 
 }
