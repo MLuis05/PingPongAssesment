badac4790c43efa8be1210d6a1c176a4ef70db22
MLuis05 <luisxd.ll85@gmail.com>
2025-07-22 14:51:13 -0600

    ValidateVehicle

--- src/main/java/ValidateVehicles/ValidateVehicles.java
+++ src/main/java/ValidateVehicles/ValidateVehicles.java
@@ -10,4 +10,36 @@ public class ValidateVehicles {
             return vehicle.getModel();
         }
     }
+
+    public String validateVehicleBrand(Vehicles vehicle) {
+        if (vehicle.getBrand() == null || vehicle.getBrand().trim().isEmpty()) {
+            return "Vehicle brand must not be empty";
+        } else {
+            return vehicle.getBrand();
+        }
+    }
+
+    public String validateVehicleYear(Vehicles vehicle) {
+        if (vehicle.getVehicleYear() == null || vehicle.getVehicleYear().trim().isEmpty()) {
+            return "Vehicle year must not be empty";
+        } else {
+            return vehicle.getVehicleYear();
+        }
+    }
+
+    public String validateVehicleColor(Vehicles vehicle) {
+        if (vehicle.getColor() == null || vehicle.getColor().trim().isEmpty()) {
+            return "Vehicle color must not be empty";
+        } else {
+            return vehicle.getColor();
+        }
+    }
+
+    public String validateVehicleVin(Vehicles vehicle) {
+        if (vehicle.getVin() == null || vehicle.getVin().trim().isEmpty()) {
+            return "Vehicle VIN must not be empty";
+        } else {
+            return vehicle.getVin();
+        }
+    }
 }
