47caa18cc35b1cdd532720feec1919601d2e07fc
Ismael-Martinez_atsolera <Ismael.Martinez@Solera.com>
2025-07-22 12:09:51 -0600

    whorkshopcontrollerError

--- src/main/java/com/SoleraBootcamp/PingPongAssesment/Controllers/WorkshopsController.java
+++ src/main/java/com/SoleraBootcamp/PingPongAssesment/Controllers/WorkshopsController.java
@@ -8,6 +8,7 @@ import com.SoleraBootcamp.PingPongAssesment.Model.Workshops;
 import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,8 +29,13 @@ public class WorkshopsController {
 
     // Endpoint to create a new workshop
     @PostMapping("/workshops")
-    public String createWorkshop(@RequestBody Workshops workShop) {
-        return workshopService.createWorkshop(workShop);
+    public ResponseEntity<String> createWorkshop(@RequestBody Workshops workShop) {
+        try{
+            String result = workshopService.createWorkshop(workShop);
+            return ResponseEntity.status(201).body(result);
+        } catch(Exception e){
+        return ResponseEntity.status(500).body("Internal Server Error: " + e.getMessage());
+        }
     }
     
     // Endpoint to get a workshop by ID
